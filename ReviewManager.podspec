3957aef2f333586d01f735c179f68bc851c83a4b
Brendan Lee <brendan@52inc.com>
2017-03-29 14:11:53 -0400

    Initial commit

---
+++ ReviewManager.podspec
@@ -0,0 +1,41 @@
+#
+# Be sure to run `pod lib lint Pulley.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'ReviewManager'
+  s.version          = '1.0.0'
+  s.summary          = 'A simple class to handle fallbacks from iOS 10.3\'s review prompt.'
+
+# This description is used to generate tags and improve search results.
+#   * Think: What does it do? Why did you write it? What is the focus?
+#   * Try to keep it short, snappy and to the point.
+#   * Write the description between the DESC delimiters below.
+#   * Finally, don't worry about the indent, CocoaPods strips it!
+
+  s.description      = <<-DESC
+A simple class to handle fallbacks from iOS 10.3's review prompt and automatically forward them to the App Store if the review prompt doesn't show.
+                       DESC
+
+  s.homepage         = 'https://github.com/52inc/ReviewManager'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'Brendan Lee' => 'brendan@52inc.com' }
+  s.source           = { :git => 'https://github.com/52inc/ReviewManager.git', :tag => s.version.to_s }
+  s.social_media_url = 'https://twitter.com/52_inc'
+
+  s.ios.deployment_target = '9.0'
+
+  s.source_files = 'ReviewManagerLib/*.*'
+
+  # s.public_header_files = 'Pod/Classes/**/*.h'
+  s.frameworks = 'UIKit', 'StoreKit'
+
+  s.xcconfig = {
+    'SWIFT_VERSION' => '3.0'
+  }
+
+end
---
+++ Source/ReviewManager.swift
@@ -0,0 +1,71 @@
+//
+//  ReviewManager.swift
+//  52inc
+//
+//  Created by Brendan Lee on 3/29/17.
+//  Copyright © 2017 52inc. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+public class ReviewManager {
+    
+    static let shared: ReviewManager = ReviewManager()
+    
+    fileprivate var reviewPromptTimer: Timer? {
+        willSet {
+            if let validTimer = reviewPromptTimer, validTimer.isValid
+            {
+                validTimer.invalidate()
+            }
+        }
+    }
+    
+    fileprivate init()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(windowDidBecomeVisible(notification:)), name: .UIWindowDidBecomeVisible, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    public func promptForReview(appId: String? = nil)
+    {
+        if #available(iOS 10.3, *) {
+            SKStoreReviewController.requestReview()
+            
+            reviewPromptTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(showStoreFromTimer(timer:)), userInfo: appId, repeats: false)
+            
+        } else {
+            
+            // Fallback on earlier versions
+            if let validAppId = appId
+            {
+                openAppStore(appId: validAppId)
+            }
+        }
+    }
+    
+    public func openAppStore(appId: String)
+    {
+        UIApplication.shared.openURL(URL(string: "https://itunes.apple.com/app/id\(appId)")!)
+    }
+    
+    @objc fileprivate func showStoreFromTimer(timer: Timer)
+    {
+        if let validAppId = timer.userInfo as? String
+        {
+            openAppStore(appId: validAppId)
+        }
+    }
+
+    @objc fileprivate func windowDidBecomeVisible(notification: Notification)
+    {
+        if let window = notification.object as AnyObject?, String(describing: type(of: window)).hasPrefix("SK")
+        {
+            reviewPromptTimer = nil
+        }
+    }
+}
